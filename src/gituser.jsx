725b2dc9158ef3dbaea713cb0662553b763ea2c8
Pallavi Roy sawant <justroy@pallavis-mbp.home>
2019-01-17 16:14:20 +0000

    Git Users first draft

---
+++ src/gituser.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class GitUsers extends Component{
+
+  constructor(){
+    super();
+    this.state={users:[]};
+  }
+
+  componentDidMount(){
+    axios.get('git.json')
+    .then(({data}) => {
+     console.log(data);
+     this.setState({users:data});
+    });
+
+
+  }
+
+  render(){
+    const {users} = this.state;
+    return(
+    <div>
+    {users && users.map((user) =>
+      <table className="table">
+       <tr>
+        <td>{user.login}</td>
+       </tr>
+      </table>
+    )}
+    </div>
+    );
+
+  }
+
+
+}
+
+export default GitUsers;
